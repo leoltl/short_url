feaab44434690197dad25b13eea3afa38f06469a
Leo Lee <tl.leolee@gmail.com>
2020-07-27 13:53:59 -0700

    update table-created

--- shorturl.py
+++ shorturl.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from app import create_app, db
+from sqlalchemy import inspect
 
 load_dotenv()
 app = create_app()
@@ -7,12 +8,14 @@ app = create_app()
 from app.models import URL, User, Visit
 
 with app.app_context():
-  if (len(User.query.all()) == 0):
+  tables_created = len(inspect(db.engine).get_table_names())
+  if (tables_created and len(User.query.all()) == 0):
     # add guest as default user if database does not contain one
     guest = User().set_value(name='guest', username='guest@shortify.com', email='guest@shortify.com', password='password')
     db.session.add(guest)
     db.session.commit()
     print('Guest user is created')
+
 
 @app.shell_context_processor
 def make_shell_context():
