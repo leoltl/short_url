2b05e7c4abea4e059a6baa222fb0806e12107119
Leo Lee <tl.leolee@gmail.com>
2020-07-27 17:43:36 -0700

    update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 source venv/bin/activate
 flask db upgrade
-exec gunicorn -b :5000 --access-logfile - --error-logfile - shorturl:app
+exec gunicorn -b :5000 --access-logfile - --error-logfile - --certfile /certs/fullchain.pem --keyfile /certs/privkey.pem shorturl:app
