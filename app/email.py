a01695937ddc33c2bff425561698ebc8189bcda8
Leo Lee <tl.leolee@gmail.com>
2020-07-29 15:38:05 -0700

    refactor get location request call out to separate function

--- app/email.py
+++ app/email.py
@@ -15,7 +15,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
 
 def send_password_request_email(user):
   token = user.get_reset_password_token()
-  send_email('[Microblog] Reset Your Password',
+  send_email('[Shortify] Reset Your Password',
                sender=current_app.config['ADMINS'][0],
                recipients=[user.email],
                text_body=render_template('email/reset_password.txt',
